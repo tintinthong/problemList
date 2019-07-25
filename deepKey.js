8c22d55b6a37fa148ef754e50cbc8042789adb63
Justin Thong <justinthong93@gmail.com>
2019-07-25 18:22:12 +0800

    First commit deepKey. Object sorting basically

---
+++ deepKey.js
@@ -0,0 +1,13 @@
+  
+  //Sort this object based on the length of inArray
+  
+  const arrayRefine = {
+    'j': {
+      inArray:[9,9],
+      outArray:[1,2,3]
+    },
+    't': { 
+      inArray: [ 11, 11, 11 ], 
+      outArray: [ 2, 3 ] 
+    }
+  };
